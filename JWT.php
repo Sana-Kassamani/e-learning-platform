a763ddc9c98a37b4f488b454fd99a7c32b9b6dd5
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-16 15:43:16 +0200

    PHP <> Create - dummy jwt file

---
+++ JWT.php
@@ -0,0 +1,10 @@
+<?php
+
+function createJWT($user_id,$user_type){
+    return "JWTString";
+
+}
+function verifyJWT($jwtString){
+    $user_id =1;
+    return $user_id;
+}
--- verifyLogin.php
+++ verifyLogin.php
@@ -1,11 +1,18 @@
 <?php
 
 include "connection.php";
+include "JWT.php";
 
 $username=$_POST["username"];
 $password= $_POST["password"];
+$user_id = 1;
+$user_type ="student";
+$jwt_string = createJWT($user_id,$user_type);
 
+
+http_response_code(200);
 // verify in db
 echo json_encode([
-    "message"=>"Login Successful"
+    "message"=>"Login Successful",
+    "jwt"=> $jwt_string
 ]);
