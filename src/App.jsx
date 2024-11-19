0bf31398232220d5702abc05f28b8aa95b596d15
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-19 07:08:00 +0200

    React <> Create - Admin page with all courses (untested)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import Login from "./pages/Login";
 import StudentHome from "./pages/StudentHome";
 import InstructorHome from "./pages/InstructorHome";
+import AdminHome from "./pages/AdminHome";
 import InstructorCourse from "./pages/InstructorCourse";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Course from "./pages/Course";
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/student" element={<StudentHome />} />
       <Route path="/instructor" element={<InstructorHome />} />
+      <Route path="/admin" element={<AdminHome />} />
       <Route path="/course" element={<Course />} />
       <Route path="/instructor-course" element={<InstructorCourse />} />
     </Routes>
---
+++ src/pages/AdminHome.jsx
@@ -0,0 +1,94 @@
+import "../styles/base/utilities.css";
+import "../styles/student.css";
+import axios from "axios";
+import Header from "../components/Header";
+import Courses from "../components/Courses";
+import { useNavigate } from "react-router-dom";
+import { request } from "../utils/request";
+import React, { useState, useEffect } from "react";
+
+const AdminHome = () => {
+  const { error, setError } = useState("");
+  const [courses, setCourses] = useState([]);
+  const [courseForm, setCourseForm] = useState({
+    title: "",
+    description: "",
+  });
+
+  const loadCourses = async () => {
+    const response = await request({
+      route: "getAllCourses",
+    });
+    console.log(response.data);
+
+    setCourses(response.data.courses);
+  };
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+  return (
+    <>
+      <Header />
+      <h2>All Courses</h2>
+      <Courses courses={courses} />
+      <div className="flex column add-form ">
+        <input
+          type="text"
+          placeholder="Title of course"
+          onChange={(e) => {
+            setCourseForm((prev) => {
+              return {
+                ...prev,
+                title: e.target.value,
+              };
+            });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Description of course"
+          onChange={(e) => {
+            setCourseForm((prev) => {
+              return {
+                ...prev,
+                description: e.target.value,
+              };
+            });
+          }}
+        />
+
+        {error && <p>{error}</p>}
+        <button
+          onClick={async () => {
+            if (!courseForm.title || !courseForm.description) {
+              console.log("empty credentials");
+              setError("All fields are required");
+              return;
+            }
+            const data = new FormData();
+            data.append("title", courseForm.title);
+            data.append("description", courseForm.description);
+            try {
+              const response = await request({
+                body: data,
+                method: "POST",
+                route: "addCourse",
+              });
+              if (response.status === 200) {
+                console.log("Added Course");
+              }
+            } catch (error) {
+              setError(error.response.data.message);
+              console.log(error.response.data.message);
+            }
+          }}
+        >
+          Add Course
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default AdminHome;
