0324f8fe7b234d6270a46bb175ea8036afee983c
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-19 03:17:06 +0200

    JS <> Edit - header component to call api correctly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import "../styles/base/utilities.css";
 import "../styles/header.css";
+import { request } from "../utils/request";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
@@ -7,24 +8,19 @@ import React, { useState, useEffect } from "react";
 const Header = () => {
   const [name, setName] = useState("");
   const navigate = useNavigate();
-  function getName() {
-    const token = localStorage.getItem("jwtToken");
-    const name = axios
-      .get("http://localhost/e-learning-platform/getName.php", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          return response.data.name;
-        } else {
-          console.log("Error getting name");
-        }
-      })
-      .catch((error) => {
-        console.log("Server error getting name");
+  async function getName() {
+    try {
+      const response = await request({
+        route: "getName",
       });
+      if (response.status === 200) {
+        return response.data.name;
+      } else {
+        console.log(response.data.message);
+      }
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
     return name;
   }
   useEffect(() => {
