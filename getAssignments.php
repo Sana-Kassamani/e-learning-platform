391d8795b4dacca2478284df35b473d1e15f0f00
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-19 04:55:20 +0200

    PHP <> Create - get assignmnets api for a specific course

---
+++ getAssignments.php
@@ -0,0 +1,48 @@
+<?php 
+
+include "connection.php";
+include "JWT.php";
+
+$headers = getallheaders();
+$jwt = $headers["Authorization"];
+
+    $payload=verifyJWT($jwt);
+    $course_id=$_POST["course_id"];
+    
+    $query = $connection->prepare("SELECT * FROM assignments WHERE course_id =?;");
+    $query->bind_param("i", $course_id);
+    $query->execute();
+    $result = $query->get_result();
+
+if($result->num_rows != 0) {
+    $assignments = [];
+
+    while($assign = $result->fetch_assoc())
+        {
+            $assignments[]=$assign;
+        }
+        
+    http_response_code(200);
+    echo json_encode([
+    "message" => "Assignmnets retrieved successfully",
+    "assignments"=>$assignments
+    ]);
+}
+    
+else {
+        http_response_code(404);
+    
+        echo json_encode([
+        "message" => "Assignments not found"
+        ]);
+    }
+
+
+// catch(\Throwable $err){
+//     http_response_code(401);
+//     echo json_encode([
+//         "error"=> $err,
+//         "message"=> "Access Declined"
+//     ]);
+
+// }
