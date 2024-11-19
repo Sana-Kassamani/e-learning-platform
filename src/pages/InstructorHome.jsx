5b794709c12eca40a0edc7b76fb350600279536b
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-19 06:37:10 +0200

    React <> Add - request function to get courses at instructor

--- src/pages/InstructorHome.jsx
+++ src/pages/InstructorHome.jsx
@@ -1,6 +1,6 @@
 import "../styles/base/utilities.css";
 import "../styles/student.css";
-import axios from "axios";
+import { request } from "../utils/request";
 import Header from "../components/Header";
 import InstructorCourses from "../components/InstructorCourses";
 import { useNavigate } from "react-router-dom";
@@ -10,15 +10,9 @@ const InstructorHome = () => {
   const [courses, setCourses] = useState([]);
 
   const loadCourses = async () => {
-    const token = localStorage.getItem("jwtToken");
-    const response = await axios.get(
-      "http://localhost/e-learning-platform/getInstructorCourses.php",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const response = await request({
+      route: "getInstructorCourses",
+    });
     console.log(response.data);
 
     setCourses(response.data.courses);
