14bc0776f72c4d6e7b896e60f722c51e089cb92e
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-21 13:14:41 +0200

    React <> Add - functionality to add instructor at admin panel

--- src/components/InstructorsAdmin.jsx
+++ src/components/InstructorsAdmin.jsx
@@ -6,6 +6,7 @@ import { request } from "../utils/request";
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import AssignInstructor from "./AssignInstructor";
+import AddInstructor from "./AddInstructor";
 
 const InstructorsAdmin = ({ courses }) => {
   const [error, setError] = useState("");
@@ -33,95 +34,9 @@ const InstructorsAdmin = ({ courses }) => {
 
   return (
     <>
-      <Instructor instructors={instructors} />
+      <Instructor instructors={instructors} setInstructors={setInstructors} />
       <div className="flex align-center justify-center gap10">
-        <div className="flex column add-form ">
-          <input
-            type="text"
-            placeholder="Username"
-            onChange={(e) => {
-              setForm((prev) => {
-                return {
-                  ...prev,
-                  username: e.target.value,
-                };
-              });
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Password"
-            onChange={(e) => {
-              setForm((prev) => {
-                return {
-                  ...prev,
-                  password: e.target.value,
-                };
-              });
-            }}
-          />
-          <input
-            type="text"
-            placeholder="First Name of Instructor"
-            onChange={(e) => {
-              setForm((prev) => {
-                return {
-                  ...prev,
-                  first_name: e.target.value,
-                };
-              });
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Last Name of Instructor"
-            onChange={(e) => {
-              setForm((prev) => {
-                return {
-                  ...prev,
-                  last_name: e.target.value,
-                };
-              });
-            }}
-          />
-
-          {error && <p>{error}</p>}
-          <button
-            onClick={async () => {
-              for (let i in form) {
-                if (!i) {
-                  console.log("empty credentials");
-                  setError("All fields are required");
-                  return;
-                }
-              }
-              const data = new FormData();
-              data.append("first_name", form.first_name);
-              data.append("last_name", form.last_name);
-              data.append("username", form.username);
-              data.append("password", form.password);
-              try {
-                const response = await request({
-                  body: data,
-                  method: "POST",
-                  route: "addInstructor",
-                });
-                if (response.status === 200) {
-                  console.log("Added Instructor");
-                  setInstructors((prev) => {
-                    return [...prev, response.data.inserted_row];
-                  });
-                }
-              } catch (error) {
-                setError(error.response.data.message);
-                console.log(error.response.data.message);
-              }
-            }}
-          >
-            Add Instructor
-          </button>
-        </div>
-
+        <AddInstructor setInstructors={setInstructors} />
         <AssignInstructor instructors={instructors} courses={courses} />
       </div>
     </>
